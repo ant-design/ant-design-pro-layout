3d21f1655e50317691b9b7500f5cee70da623191
codeif <me@codeif.com>
2019-07-16 11:49:54 +0800

    面包屑跳转问题

--- example/src/layouts/BasicLayout.tsx
+++ example/src/layouts/BasicLayout.tsx
@@ -44,6 +44,14 @@ const BasicLayout: React.FC<BasicLayoutProps> = props => {
             <Link to={menuItemProps.path}>{defaultDom}</Link>
           )
         }
+        itemRender={(route, params, routes, paths) => {
+          const first = routes.indexOf(route) === 0;
+          return first ? (
+            <Link to={paths.join('/')}>{route.breadcrumbName}</Link>
+          ) : (
+            <span>{route.breadcrumbName}</span>
+          );
+        }}
         rightContentRender={rightProps => (
           <RightContent {...rightProps} {...settings} />
         )}
